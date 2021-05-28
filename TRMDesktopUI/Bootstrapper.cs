2acb4588888f4af202e790a096288fa8a165a25a
Waqas Ahmed <waqas809@gmail.com>
2021-05-28 14:24:52 +0500

    Adds TRMDesktopUI Project
    Adds and configures TRMDesktopUI project. Caliburn.Micro MVVM framework is also added and configured.

---
+++ TRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
