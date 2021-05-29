77d3f9cd0dc152ee7f41613a9236367d07bff30d
Waqas Ahmed <waqas809@gmail.com>
2021-05-29 12:35:16 +0500

    Getting User Data
    spUserLookup stored procedure added. TRMDataManager.Library project created to manage the db calls. API endpoint to get user data created.

--- TRMData/dbo/Tables/Inventory.sql
+++ TRMData/dbo/Tables/Inventory.sql
@@ -4,6 +4,6 @@
     [ProductId] INT NOT NULL, 
     [Quantity] INT NOT NULL DEFAULT 1, 
     [PurchasePrice] MONEY NOT NULL, 
-    [PurchaseDate] DATETIME2 NOT NULL DEFAULT getutchdate(),
+    [PurchaseDate] DATETIME2 NOT NULL DEFAULT getutcdate(),
 
 )
---
+++ TRMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using TRMDataManager.Library.Internal.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "TRMData");
+
+            return output;
+        }
+    }
+}
