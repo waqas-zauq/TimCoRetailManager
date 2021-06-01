9d4aa327f4133511839b65bd4430b85125a996db
Waqas Ahmed <waqas809@gmail.com>
2021-06-01 13:23:41 +0500

    Update EF Database Name
    Updates EF database name to be EFData.

---
+++ TRMDataManager/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace TRMDataManager.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TRMDataManager.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TRMDataManager.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- TRMDataManager/Models/IdentityModels.cs
+++ TRMDataManager/Models/IdentityModels.cs
@@ -1,8 +1,7 @@
-﻿using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
+﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity.Owin;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace TRMDataManager.Models
 {
@@ -21,10 +20,10 @@ namespace TRMDataManager.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("EFData", throwIfV1Schema: false)
         {
         }
-        
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
